7a0a2486db913e00fd73e680af8758b5d9b0dfea
ahmad <ahmadalibaffa@gmail.com>
2024-10-13 17:03:36 +0500

    Started working on Models and Created tourModel

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB Connection Successful'))
+  .catch(err => {
+    console.log('Databse Connection Failed:: ', err.message);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
