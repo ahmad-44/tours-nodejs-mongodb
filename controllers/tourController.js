05d381e25c7212f35ea9a9288d7747b3c295e754
ahmad-44 <ahmadalibaffa@gmail.com>
2024-10-19 21:37:17 +0500

    Script for importing data to DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -77,7 +77,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid Data Sent'
+      message: err
     });
   }
 };
@@ -102,9 +102,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
